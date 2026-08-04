8c11addd90bc764b815df2f4fb8bbe282d4c9642
prashant4840 <prashantsharma4840@icloud.com>
2026-08-04 15:47:14 +0530

    array question in class

---
+++ JAVA DSA/src/Arrays/PrimeNum.java
@@ -0,0 +1,25 @@
+package Arrays;
+
+public class PrimeNum {
+    static void primeNumber(int[] arr) {
+
+        for (int i = 0; i < arr.length; i++) {
+            int count = 0;
+
+            for (int j = 1; j <= arr[i]; j++) {
+                if (arr[i] % j == 0) count++;
+            }
+
+            if (count == 2) System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {10, 7, 15, 11, 20, 13, 8};
+
+        System.out.print("Prime Numbers = ");
+
+        primeNumber(arr);
+    }
+}
---
+++ JAVA DSA/src/Arrays/SwapElement.java
@@ -0,0 +1,30 @@
+package Arrays;
+
+public class SwapElement {
+    static void swap(int[] arr) {
+
+        int temp = arr[0];
+        arr[0] = arr[4];
+        arr[4] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50};
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        swap(arr);
+
+        System.out.println("\n");
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
+
+
+
+
