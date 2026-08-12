6fb828ffefed5fa2d591696c6417f02967c0b3a2
prashant4840 <prashantsharma4840@icloud.com>
2026-08-12 09:33:38 +0530

    class-problems

--- JAVA-DSA/src/Arrays/leetcode/src/Leetcode_1365.java
+++ JAVA-DSA/src/Arrays/leetcode/src/Leetcode_1365.java
@@ -1,28 +1,21 @@
 import java.util.*;
-
 public class Leetcode_1365 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
         int[] nums = new int[n];
-
         for (int i = 0; i < n; i++)
             nums[i] = sc.nextInt();
-
         int[] result = new int[n];
-
         for (int i = 0; i < n; i++) {
             int count = 0;
-
             for (int j = 0; j < n; j++) {
                 if (nums[j] < nums[i])
                     count++;
             }
-
             result[i] = count;
         }
-
         System.out.println(Arrays.toString(result));
     }
 }
